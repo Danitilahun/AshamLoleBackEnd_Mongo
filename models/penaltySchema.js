801acb38e9e882f1367c1e0f412ff34beb22ad43
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 00:02:26 +0300

    Adding penaltySchema

---
+++ models/penaltySchema.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const penaltySchema = new mongoose.Schema(
+  {
+    active: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    branchId: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    employeeId: {
+      type: String,
+      required: true,
+    },
+    employeeName: {
+      type: String,
+      required: true,
+    },
+    placement: {
+      type: String,
+      required: true,
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const PenaltySchema = mongoose.model("Penalty", penaltySchema);
+
+module.exports = PenaltySchema;
