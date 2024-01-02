242bf53b4278c3d126a19b5fd234a2cc3f0bd662
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-02 22:06:07 +0300

    adding branch total

---
+++ models/branchTotalCredit.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const branchTotalCreditSchema = new mongoose.Schema({
+  branchId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Branch",
+  },
+  customerCredit: {
+    type: Number,
+    default: 0,
+  },
+  dailyCredit: {
+    type: Number,
+    default: 0,
+  },
+  staffCredit: {
+    type: Number,
+    default: 0,
+  },
+  total: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const BranchTotalCredit = mongoose.model(
+  "BranchTotalCredit",
+  branchTotalCreditSchema
+);
+
+module.exports = BranchTotalCredit;
