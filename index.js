e7b8b52a9ac219e071da720a5823a8fd239c9330
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-02 21:22:40 +0300

    adding middlewares

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 require("dotenv").config();
 const connectToDB = require("./config/database");
+
+const app = express();
+
+app.use(express.json({ limit: "60mb", extended: true }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
