132ef5e64601e6b96a7d762a244d7402453f30fe
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-02 23:29:08 +0300

    adding bank schema

---
+++ models/bankSchema.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const bankSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    bankName: {
+      type: String,
+      required: true,
+    },
+    branchId: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    placement: {
+      type: String,
+      required: true,
+      enum: ["DeliveryGuy"],
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+      enum: ["branch", "finance"],
+    },
+    transactionType: {
+      type: String,
+      required: true,
+      enum: ["Withdrawal", "Deposit"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Bank = mongoose.model("Bank", bankSchema);
+
+module.exports = Bank;
--- models/branchRelatedSchema/branchBankTotal.js
+++ models/branchRelatedSchema/branchBankTotal.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bankNames = require("../util/bankNames");
+const bankNames = require("../../util/bankNames");
 
 const branchBankTotalSchema = new mongoose.Schema(
   {
