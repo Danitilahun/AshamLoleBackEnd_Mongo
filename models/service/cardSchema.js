590c63c64c24f5db839387619e54c2e137691c82
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-02 23:46:19 +0300

    Adding card schema

---
+++ models/service/cardSchema.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema(
+  {
+    sheetId: {
+      type: String,
+      required: true,
+    },
+    amountBirr: {
+      type: Number,
+      required: true,
+    },
+    blockHouse: {
+      type: String,
+      required: true,
+    },
+    branchId: {
+      type: String,
+      required: true,
+    },
+    branchKey: {
+      type: String,
+      required: true,
+    },
+    branchName: {
+      type: String,
+      required: true,
+    },
+    callcenterId: {
+      type: String,
+      required: true,
+    },
+    callcenterName: {
+      type: String,
+      required: true,
+    },
+    cardBranch: {
+      type: String,
+      required: true,
+    },
+    createdDate: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    dayRemain: {
+      type: Number,
+      required: true,
+    },
+    deliveryguyId: {
+      type: String,
+      required: true,
+    },
+    deliveryguyName: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    remaingMoney: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Assigned", "Completed"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const CardSchema = mongoose.model("Card", cardSchema);
+
+module.exports = CardSchema;
