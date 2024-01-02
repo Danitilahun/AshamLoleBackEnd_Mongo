f0300e9e68fa80d17d268570950a0b24b78338da
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 00:57:47 +0300

    SheetsSchema is added

---
+++ models/sheetsSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SheetsSchema = new Schema({
+  Tables: [String],
+  active: String,
+  activeDailySummary: String,
+  branchId: String,
+  createdAt: { type: Date, default: Date.now },
+  date: String,
+  name: String,
+  prevActive: String,
+  previousActive: String,
+  realDate: String,
+  sheetNumber: Number,
+  sheetStatus: String,
+  tableDate: [String],
+  tableCount: Number,
+});
+
+const Sheets = mongoose.model("Sheets", SheetsSchema);
+
+module.exports = Sheets;
