3efba6d6b09277a01e626af803a5f0f4649e7ca3
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 00:13:53 +0300

    staffCreditSchema

---
+++ models/credit/staffCreditSchema.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const staffCreditSchema = new mongoose.Schema(
+  {
+    active: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    branchId: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    employeeId: {
+      type: String,
+      required: true,
+    },
+    employeeName: {
+      type: String,
+      required: true,
+    },
+    placement: {
+      type: String,
+      required: true,
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const StaffCreditSchema = mongoose.model("StaffCredit", staffCreditSchema);
+
+module.exports = StaffCreditSchema;
