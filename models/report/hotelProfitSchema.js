2b5accb13af57451ba79c7da19b28931c3b8d829
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 00:44:22 +0300

    Adding hotelProfitSchema

---
+++ models/report/hotelProfitSchema.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const hotelProfitSchema = new mongoose.Schema(
+  {
+    CHECK_SOURCE: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: String,
+      required: true,
+    },
+    activeDailySummery: {
+      type: String,
+      required: true,
+    },
+    activeTable: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    branchId: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    deliveryguyId: {
+      type: String,
+      required: true,
+    },
+    deliveryguyName: {
+      type: String,
+      required: true,
+    },
+    gain: {
+      type: Number,
+      required: true,
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const HotelProfit = mongoose.model("HotelProfit", hotelProfitSchema);
+
+module.exports = HotelProfit;
