00884c5cf373dc4e3a722636f9fbd58fcd3770bc
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 00:29:18 +0300

    waterSchema added

---
+++ models/service/waterSchema.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+
+const waterSchema = new mongoose.Schema(
+  {
+    active: {
+      type: String,
+      required: true,
+    },
+    activeDailySummery: {
+      type: String,
+      required: true,
+    },
+    activeTable: {
+      type: String,
+      required: true,
+    },
+    billPayerName: {
+      type: String,
+      required: true,
+    },
+    blockHouse: {
+      type: String,
+      required: true,
+    },
+    branchId: {
+      type: String,
+      required: true,
+    },
+    branchKey: {
+      type: String,
+      required: true,
+    },
+    branchName: {
+      type: String,
+      required: true,
+    },
+    callcenterId: {
+      type: String,
+      required: true,
+    },
+    callcenterName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+    createdDate: {
+      type: String,
+      required: true,
+    },
+    customerKey: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    deliveryguyId: {
+      type: String,
+      required: true,
+    },
+    deliveryguyName: {
+      type: String,
+      required: true,
+    },
+    fromWhere: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Water = mongoose.model("Water", waterSchema);
+
+module.exports = Water;
