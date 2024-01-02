a6bdbf9c94819d675cdb793c4d1c591a075b3f05
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-02 22:52:25 +0300

    creating reusable credit service

---
+++ services/common/create.doc.js
@@ -0,0 +1,24 @@
+/**
+ * Creates a new document in the specified Mongoose collection/model.
+ *
+ * @param {Model} Model - The Mongoose model representing the collection.
+ * @param {Object} data - The data to be added to the newly created document.
+ * @returns {Promise<string>} A Promise that resolves with the ID of the created document.
+ * @throws {Error} Throws an error if the operation fails.
+ *
+ */
+
+const createDocument = async (Model, data) => {
+  try {
+    const createdDocument = await Model.create(data);
+    console.log(
+      `Document successfully created in ${Model.collection.name} collection`
+    );
+    return createdDocument._id; // Return the ID of the created document
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+module.exports = createDocument;
