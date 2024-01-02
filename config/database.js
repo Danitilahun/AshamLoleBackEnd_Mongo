cac529dc5ac24164754a438f14754663585190da
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-02 21:08:32 +0300

    Adding files

---
+++ config/database.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+
+  mongoose.connection.on("connected", () => {
+    console.log("Mongoose connected to the database");
+  });
+
+  mongoose.connection.on("error", (error) => {
+    console.error("Mongoose connection error:", error.message);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose connection is disconnected");
+  });
+
+  // Return the Mongoose connection instance
+  return mongoose.connection;
+};
+
+module.exports = connectToDB;
