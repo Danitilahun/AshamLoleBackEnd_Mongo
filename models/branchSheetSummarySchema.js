7872ca4b8ff6991ad371d6e29de703d2652d8da7
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-02 21:57:55 +0300

    adding branch sheet summary schema

---
+++ models/branchSheetSummarySchema.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+// Define the schema for branchSheetSummary
+const branchSheetSummarySchema = new mongoose.Schema({
+  sheetSummary: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SheetStatus",
+    },
+  ],
+
+  budget: {
+    type: Number,
+    default: 0,
+  },
+
+  netGain: {
+    type: Number,
+    default: 0,
+  },
+
+  totalExpense: {
+    type: Number,
+    default: 0,
+  },
+
+  totalIncome: {
+    type: Number,
+    default: 0,
+  },
+});
+
+// Create and export the model
+const BranchSheetSummary = mongoose.model(
+  "BranchSheetSummary",
+  branchSheetSummarySchema
+);
+
+module.exports = BranchSheetSummary;
