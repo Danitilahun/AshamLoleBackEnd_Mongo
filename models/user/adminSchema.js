db57e3389526f3209d334d376d692f0d95c3e0bb
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 00:32:34 +0300

    adminSchema added

---
+++ models/user/adminSchema.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+  {
+    active: {
+      type: String,
+      required: true,
+    },
+    bankAccount: {
+      type: String,
+      required: true,
+    },
+    branchId: {
+      type: String,
+      required: true,
+    },
+    branchName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+    difference: {
+      type: Number,
+      required: true,
+    },
+    disable: {
+      type: Boolean,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    fullAddress: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: String,
+      required: true,
+    },
+    securityAddress: {
+      type: String,
+      required: true,
+    },
+    securityName: {
+      type: String,
+      required: true,
+    },
+    securityPhone: {
+      type: String,
+      required: true,
+    },
+    uniqueName: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
