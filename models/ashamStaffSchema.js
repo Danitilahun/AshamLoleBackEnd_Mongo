7f3175441aa7ad5fcb500f7fd5ce3cae651baa4d
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 11:20:47 +0300

    ashamStaffSchema

---
+++ models/ashamStaffSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ashamStaffSchema = new Schema({
+  branch: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+const AshamStaff = mongoose.model("AshamStaff", ashamStaffSchema);
+
+module.exports = AshamStaff;
