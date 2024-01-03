804a7f95ce2c6e78a0c2abe24599fd074768c8c1
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 19:43:07 +0300

    adding get single branch controller

---
+++ controllers/branch/getSingleBranch.js
@@ -0,0 +1,20 @@
+const Branch = require("../../models/branchRelatedSchema/branchSchema");
+
+const getSingleBranch = async (req, res) => {
+  try {
+    const branchId = req.params.id; // Get branch ID from request params
+    const branch = await Branch.findById(branchId);
+
+    if (!branch) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Branch not found" });
+    }
+
+    res.status(200).json({ success: true, data: branch });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+module.exports = getSingleBranch;
