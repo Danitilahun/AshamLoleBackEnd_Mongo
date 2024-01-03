34da298bd4856fc8e87497916d151ec2475a2ba1
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-03 19:49:02 +0300

    adding routes

--- routes/branch/route.js
+++ routes/branch/route.js
@@ -3,10 +3,15 @@ const express = require("express");
 const createBranch = require("../../controllers/branch/create");
 const editBranch = require("../../controllers/branch/edit");
 const deleteBranch = require("../../controllers/branch/delete");
+const getAllBranches = require("../../controllers/branch/getAllbranches");
+const getSingleBranch = require("../../controllers/branch/getSingleBranch");
 const router = express.Router();
 
 // Define the route for creating data for an admin
+router.get("/", getAllBranches);
+router.get("/:id", getSingleBranch);
 router.post("/", createBranch);
 router.put("/:id", editBranch);
 router.delete("/:id", deleteBranch);
+
 module.exports = router;
