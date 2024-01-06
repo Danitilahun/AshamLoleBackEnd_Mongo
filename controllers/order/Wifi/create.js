4dcef13ad79317bceb365024a990c5c91d13fcae
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-07 00:48:53 +0300

    Create a new endpoint to create Wifi records and corresponding Customer entries in a transactional manner. This function uses the Wifi model to create new Wifi records and sets up associated Customer data with the Wifi information.

---
+++ controllers/order/Wifi/create.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Wifi = require("../../../models/service/wifiSchema");
+const Customer = require("../../../models/customerSchema");
+
+const createCustomerAndWifi = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { blockHouse, branchId, branchName, name, phone } = req.body;
+
+    const newWifi = new Wifi(req.body);
+
+    const newCustomer = new Customer({
+      Wifi: "YES",
+      blockHouse,
+      branchId,
+      branchName,
+      blockHouse,
+      name,
+      phone,
+      orderId: newWifi._id,
+    });
+
+    await newWifi.save({ session });
+    await newCustomer.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      message: "Wifi created successfully",
+      newWifi,
+      newCustomer,
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = createCustomerAndWifi;
