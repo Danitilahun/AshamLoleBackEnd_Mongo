3377b797eeef8de43573530ceed9477eb80fb2f4
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-06 21:33:28 +0300

    Adding a function for get All essential docuement

---
+++ controllers/essentials/get.js
@@ -0,0 +1,25 @@
+const Essential = require("../../models/essentialSchema");
+
+const getAllEssentials = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; // Get the page number from the query parameters or default to 1
+    const limit = 10; // Number of documents to retrieve per page
+    const skip = (page - 1) * limit; // Calculate the number of documents to skip
+
+    const totalDocs = await Essential.countDocuments(); // Get the total count of documents
+
+    const essentials = await Essential.find().skip(skip).limit(limit);
+
+    res.status(200).json({
+      page,
+      totalPages: Math.ceil(totalDocs / limit),
+      count: essentials.length,
+      data: essentials,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = getAllEssentials;
