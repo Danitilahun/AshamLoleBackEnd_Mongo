ed11d4bded42234945cfe208412f9ee550bc8cee
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-10 20:09:51 +0300

    feat: Implement editAdmin controller function

---
+++ controllers/users/admin/edit.js
@@ -0,0 +1,57 @@
+// Edit an existing admin
+const editAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      sheetId,
+      bankAccount,
+      branchId,
+      branchName,
+      difference,
+      disable,
+      email,
+      fullAddress,
+      fullName,
+      phone,
+      profileImage,
+      salary,
+      securityAddress,
+      securityName,
+      securityPhone,
+      uniqueName,
+    } = req.body;
+
+    const updatedAdmin = await Admin.findByIdAndUpdate(
+      id,
+      {
+        sheetId,
+        bankAccount,
+        branchId,
+        branchName,
+        difference,
+        disable,
+        email,
+        fullAddress,
+        fullName,
+        phone,
+        profileImage,
+        salary,
+        securityAddress,
+        securityName,
+        securityPhone,
+        uniqueName,
+      },
+      { new: true }
+    );
+
+    if (!updatedAdmin) {
+      return res.status(404).json({ message: "Admin not found" });
+    }
+
+    res.status(200).json(updatedAdmin);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = editAdmin;
