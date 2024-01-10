7a32b51a9e506f9acb68882489f42c759cbe32e4
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-10 19:52:37 +0300

    feat: Implement editBonus controller function

---
+++ controllers/incentive/bonus/edit.js
@@ -0,0 +1,41 @@
+const Bonus = require("../../../models/incentive/bonusSchema");
+
+// Edit an existing bonus
+const editBonus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      sheetId,
+      amount,
+      branchId,
+      date,
+      employeeId,
+      employeeName,
+      placement,
+      reason,
+    } = req.body;
+
+    const updatedBonus = await Bonus.findByIdAndUpdate(
+      id,
+      {
+        sheetId,
+        amount,
+        branchId,
+        date,
+        employeeId,
+        employeeName,
+        placement,
+        reason,
+      },
+      { new: true }
+    );
+
+    if (!updatedBonus) {
+      return res.status(404).json({ message: "Bonus not found" });
+    }
+
+    res.status(200).json(updatedBonus);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
