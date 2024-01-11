4aa1de8a822b565399fd34e77e54b8754eab4954
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-11 21:13:50 +0300

    update daily credit schema

--- models/credit/dailyCreditSchema.js
+++ models/credit/dailyCreditSchema.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const DailyCreditSchema = new Schema({
+  uniqueDailyCreditId: {
+    type: String,
+    required: true,
+  },
   sheetId: {
     type: String,
     required: true,
