9d4017795a272b431fa99b33d2c4037b69642108
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-12 19:02:49 +0300

    feat: implement createPenalty for updating penalties in salary tables
    This commit introduces the createPenalty function to handle the creation of penalties, updating the respective penalty amount in either the delivery guy's or staff member's salary table based on the placement. The code includes session and transaction support for data consistency. This addition enhances the system's ability to manage penalties efficiently for both delivery guys and staff members.

--- controllers/incentive/penality/create.js
+++ controllers/incentive/penality/create.js
@@ -1,32 +1,51 @@
-const Penalty = require("../../../models/incentive/penaltySchema");
+const { startSession } = require("mongoose");
+const updateDeliveryGuySalaryTable = require("../../../services/sheetRelated/update/updateDeliveryGuySalaryTable");
+const updateStaffSalaryTableEntry = require("../../../services/sheetRelated/update/updateStaffSalaryTableEntry");
+const Branch = require("../../../models/branchRelatedSchema/branchSchema");
+
 // Create a new penalty
 const createPenalty = async (req, res) => {
+  const session = await startSession();
+  session.startTransaction();
+
   try {
-    const {
-      active,
-      amount,
-      branchId,
-      date,
-      employeeId,
-      employeeName,
-      placement,
-      reason,
-    } = req.body;
+    const data = req.body;
+    const branch = await Branch.findById(data.branchId);
 
-    const newPenalty = new Penalty({
-      active,
-      amount,
-      branchId,
-      date,
-      employeeId,
-      employeeName,
-      placement,
-      reason,
-    });
+    // Create a new penalty document within the session
 
-    const savedPenalty = await newPenalty.save();
+    const newPenalty = new Penalty(data);
+    const savedPenalty = await newPenalty.save({ session });
+
+    if (data.placement === "DeliveryGuy") {
+      await updateDeliveryGuySalaryTable(
+        branch.activeDeliverySalaryTable,
+        data.employeeId,
+        "penalty",
+        parseFloat(data.amount ? data.amount : 0),
+        parseFloat(data.amount ? data.amount : 0),
+        session
+      );
+    } else {
+      await updateStaffSalaryTableEntry(
+        branch.activeStaffSalarySheet,
+        data.employeeId,
+        "penalty",
+        parseFloat(data.amount ? data.amount : 0),
+        parseFloat(data.amount ? data.amount : 0),
+        session
+      );
+    }
+    // Commit the transaction if successful
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(201).json(savedPenalty);
   } catch (error) {
+    // Abort the transaction and end the session in case of an error
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
