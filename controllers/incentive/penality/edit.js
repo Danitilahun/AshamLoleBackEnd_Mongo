d9091253de9d1d3381d38679b64400cdf45c0cd6
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-12 19:10:04 +0300

    feat: implement editPenalty for updating penalties and salary tables
    This commit introduces the editPenalty function, allowing the modification of existing penalty records. The code calculates the difference in penalty amounts, updates the penalty document, and synchronously adjusts the respective salary table based on the placement (DeliveryGuy or Staff). The implementation includes session and transaction support for data consistency, providing an efficient way to manage penalty updates in the system.

--- controllers/incentive/penality/edit.js
+++ controllers/incentive/penality/edit.js
@@ -1,41 +1,64 @@
-const Penalty = require("../../../models/incentive/penaltySchema");
+const { startSession } = require("mongoose");
+const updateDeliveryGuySalaryTable = require("../../../services/sheetRelated/update/updateDeliveryGuySalaryTable");
+const updateStaffSalaryTableEntry = require("../../../services/sheetRelated/update/updateStaffSalaryTableEntry");
+const Branch = require("../../../models/branchRelatedSchema/branchSchema");
 
 // Edit an existing penalty
 const editPenalty = async (req, res) => {
+  const session = await startSession();
+  session.startTransaction();
+
   try {
     const { id } = req.params;
-    const {
-      active,
-      amount,
-      branchId,
-      date,
-      employeeId,
-      employeeName,
-      placement,
-      reason,
-    } = req.body;
+    const data = req.body;
+    const branch = await Branch.findById(data.branchId);
 
-    const updatedPenalty = await Penalty.findByIdAndUpdate(
-      id,
-      {
-        active,
-        amount,
-        branchId,
-        date,
-        employeeId,
-        employeeName,
-        placement,
-        reason,
-      },
-      { new: true }
-    );
+    // Find the existing penalty document by ID within the session
+    const existingPenalty = await Penalty.findById(id).session(session);
 
-    if (!updatedPenalty) {
-      return res.status(404).json({ message: "Penalty not found" });
+    if (!existingPenalty) {
+      throw new Error("Penalty not found for the given ID");
     }
+
+    // Calculate the difference in penalty amounts
+    const penaltyDifference =
+      parseFloat(data.amount ? data.amount : 0) - existingPenalty.amount;
+
+    // Update the existing penalty document with new data
+    Object.assign(existingPenalty, data);
+    const updatedPenalty = await existingPenalty.save({ session });
+
+    // Update the salary table based on placement with the difference
+    if (data.placement === "DeliveryGuy") {
+      await updateDeliveryGuySalaryTable(
+        branch.activeDeliverySalaryTable,
+        data.employeeId,
+        "penalty",
+        -penaltyDifference,
+        -penaltyDifference,
+        session
+      );
+    } else {
+      await updateStaffSalaryTableEntry(
+        branch.activeStaffSalarySheet,
+        data.employeeId,
+        "penalty",
+        -penaltyDifference,
+        -penaltyDifference,
+        session
+      );
+    }
+
+    // Commit the transaction if successful
+    await session.commitTransaction();
+    session.endSession();
 
     res.status(200).json(updatedPenalty);
   } catch (error) {
+    // Abort the transaction and end the session in case of an error
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
