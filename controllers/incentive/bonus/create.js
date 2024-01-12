eb8cb87f944714c29a90befa3b21afb1cb14dd50
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-12 18:12:49 +0300

    feat: implement session and transaction handling in createBonus
    This commit introduces session and transaction handling for MongoDB operations in the createBonus function. Sessions are used to ensure atomicity and consistency during database operations. The code has been updated to start, commit, and end the session appropriately. Additionally, the function now supports creating a new bonus document and updating the relevant salary table based on the employee's placement (DeliveryGuy or Staff). The bonus amount is deducted from the respective salary table. This enhances data integrity and error handling in the bonus creation process.

--- controllers/incentive/bonus/create.js
+++ controllers/incentive/bonus/create.js
@@ -1,5 +1,8 @@
 const { startSession } = require("mongoose");
 const Bonus = require("../../../models/incentive/bonusSchema");
+const updateDeliveryGuySalaryTable = require("../../../services/sheetRelated/update/updateDeliveryGuySalaryTable");
+const updateStaffSalaryTableEntry = require("../../../services/sheetRelated/update/updateStaffSalaryTableEntry");
+const Branch = require("../../../models/branchRelatedSchema/branchSchema");
 
 // Create a new bonus
 const createBonus = async (req, res) => {
@@ -8,11 +11,31 @@ const createBonus = async (req, res) => {
 
   try {
     const data = req.body;
-
+    const branch = await Branch.findById(data.branchId);
     // Create a new bonus document within the session
+
     const newBonus = new Bonus(data);
     const savedBonus = await newBonus.save({ session });
 
+    if (data.placement === "DeliveryGuy") {
+      await updateDeliveryGuySalaryTable(
+        branch.activeDeliverySalaryTable,
+        data.deliveryguyId,
+        "bonus",
+        parseFloat(data.amount ? data.amount : 0),
+        -parseFloat(data.amount ? data.amount : 0),
+        session
+      );
+    } else {
+      await updateStaffSalaryTableEntry(
+        branch.activeStaffSalarySheet,
+        data.employeeId,
+        "bonus",
+        parseFloat(data.amount ? data.amount : 0),
+        -parseFloat(data.amount ? data.amount : 0),
+        session
+      );
+    }
     // Commit the transaction if successful
     await session.commitTransaction();
     session.endSession();
