10327ac5ea290cb241904a0910721aad9e7433a6
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-12 15:51:50 +0300

    feat: Add waterDistribute function
    Introduce the waterDistribute function to handle the distribution of water-related data, updating daily tables, 15 days summaries, and individual daily work summaries. The function calculates distribution based on the water distribution gain from the CompanyGain document.

--- controllers/credit/dailyCredit/reportDelete/waterDistribute.js
+++ controllers/credit/dailyCredit/reportDelete/waterDistribute.js
@@ -3,51 +3,49 @@ const updateDeliveryGuy15DayWorkSummary = require("../../../../services/sheetRel
 const updateFieldInFifteenDayWorkSummary = require("../../../../services/sheetRelated/update/updateFieldInFifteenDayWorkSummary");
 const updateDailyTableEntry = require("../../../../services/tableRelated/updateDailyTableEntry");
 
-const wifiDistribute = async (data, session) => {
+const waterDistribute = async (data, session) => {
   try {
     const companyGainDoc = await CompanyGain.findOne();
-    const wifiDistributeGain = companyGainDoc.wifi_distribute_gain;
-    if (!wifiDistributeGain) {
+    const waterDistributeGain = companyGainDoc.water_distribute_gain;
+    if (!waterDistributeGain) {
       throw new Error(
-        "Company gain information is missing.Please refresh your browser and try again."
+        "Company gain information is missing. Please refresh your browser and try again."
       );
     }
-    // First update: Change the daily table
 
+    // First update: Change the daily table
     await updateDailyTableEntry(
       data.activeTable,
       data.deliveryguyId,
-      "wifiDistribute",
+      "waterDistribute",
       data.numberOfCard,
-      data.numberOfCard * wifiDistributeGain,
+      data.numberOfCard * waterDistributeGain,
       session
     );
 
     // Second update: Change the 15 days summary and daily summary tables
-
     await updateDeliveryGuy15DayWorkSummary(
       data.activeDailySummery,
       data.deliveryguyId,
-      "wifiDistribute",
+      "waterDistribute",
       data.numberOfCard,
-      data.numberOfCard * wifiDistributeGain,
+      data.numberOfCard * waterDistributeGain,
       session
     );
 
     // Third update: Individual person's daily work summary
-
     await updateFieldInFifteenDayWorkSummary(
       session,
       data.activeDGSummery,
       data.date,
-      "wifiDistribute",
+      "waterDistribute",
       data.numberOfCard,
-      data.numberOfCard * wifiDistributeGain
+      data.numberOfCard * waterDistributeGain
     );
   } catch (error) {
-    console.error("Error in wifiDistribute:", error);
+    console.error("Error in waterDistribute:", error);
     throw error;
   }
 };
 
-module.exports = wifiDistribute;
+module.exports = waterDistribute;
