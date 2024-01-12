e96d7303e1d48c5e324994cf337726d8c2de2252
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-12 18:19:28 +0300

    feat: implement session and transaction handling in createBonus
    This commit introduces session and transaction handling for MongoDB operations in the createBonus function. Sessions are used to ensure atomicity and consistency during database operations. The code has been updated to start, commit, and end the session appropriately. Additionally, the function now supports creating a new bonus document and updating the relevant salary table based on the employee's placement (DeliveryGuy or Staff). The bonus amount is deducted from the respective salary table. This enhances data integrity and error handling in the bonus creation process.

--- controllers/incentive/bonus/edit.js
+++ controllers/incentive/bonus/edit.js
@@ -1,5 +1,8 @@
-const Bonus = require("../../../models/incentive/bonusSchema");
 const { startSession } = require("mongoose");
+const Bonus = require("../../../models/incentive/bonusSchema");
+const updateDeliveryGuySalaryTable = require("../../../services/sheetRelated/update/updateDeliveryGuySalaryTable");
+const updateStaffSalaryTableEntry = require("../../../services/sheetRelated/update/updateStaffSalaryTableEntry");
+const Branch = require("../../../models/branchRelatedSchema/branchSchema");
 
 // Edit an existing bonus
 const editBonus = async (req, res) => {
@@ -9,15 +12,43 @@ const editBonus = async (req, res) => {
   try {
     const { id } = req.params;
     const data = req.body;
-    // Find the existing bonus document by ID
+    const branch = await Branch.findById(data.branchId);
+
+    // Find the existing bonus document by ID within the session
     const existingBonus = await Bonus.findById(id).session(session);
 
     if (!existingBonus) {
       throw new Error("Bonus not found for the given ID");
     }
 
+    // Calculate the difference in bonus values
+    const bonusDifference =
+      parseFloat(data.amount ? data.amount : 0) - existingBonus.amount;
+
+    // Update the existing bonus document with new data
     Object.assign(existingBonus, data);
     const updatedBonus = await existingBonus.save({ session });
+
+    // Update the salary table based on placement with the difference
+    if (data.placement === "DeliveryGuy") {
+      await updateDeliveryGuySalaryTable(
+        branch.activeDeliverySalaryTable,
+        data.deliveryguyId,
+        "bonus",
+        bonusDifference,
+        -bonusDifference,
+        session
+      );
+    } else {
+      await updateStaffSalaryTableEntry(
+        branch.activeStaffSalarySheet,
+        data.employeeId,
+        "bonus",
+        bonusDifference,
+        -bonusDifference,
+        session
+      );
+    }
 
     // Commit the transaction if successful
     await session.commitTransaction();
@@ -25,6 +56,10 @@ const editBonus = async (req, res) => {
 
     res.status(200).json(updatedBonus);
   } catch (error) {
+    // Abort the transaction and end the session in case of an error
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
