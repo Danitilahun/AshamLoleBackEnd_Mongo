f6c80bf3fd1ee1af987e542541f57c366c3fa122
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-12 19:11:27 +0300

    feat: implement deletePenalty for removing penalties and updating salary tables
    This commit introduces the deletePenalty function, enabling the removal of existing penalty records. The code finds the penalty document by ID, deletes it within a session, and updates the respective salary table based on the placement (DeliveryGuy or Staff) with the negative penalty amount. The implementation includes session and transaction support for data consistency, providing an effective way to manage penalty deletions in the system.

--- controllers/incentive/penality/delete.js
+++ controllers/incentive/penality/delete.js
@@ -1,17 +1,57 @@
-const Penalty = require("../../../models/incentive/penaltySchema");
+const { startSession } = require("mongoose");
+const updateDeliveryGuySalaryTable = require("../../../services/sheetRelated/update/updateDeliveryGuySalaryTable");
+const updateStaffSalaryTableEntry = require("../../../services/sheetRelated/update/updateStaffSalaryTableEntry");
+const Branch = require("../../../models/branchRelatedSchema/branchSchema");
 
 // Delete a penalty
 const deletePenalty = async (req, res) => {
+  const session = await startSession();
+  session.startTransaction();
+
   try {
     const { id } = req.params;
-    const deletedPenalty = await Penalty.findByIdAndDelete(id);
 
+    // Find the existing penalty document by ID within the session
+    const deletedPenalty = await Penalty.findByIdAndDelete(id).session(session);
+    const branch = await Branch.findById(deletedPenalty.branchId);
     if (!deletedPenalty) {
+      // Abort the transaction and end the session if penalty not found
+      await session.abortTransaction();
+      session.endSession();
       return res.status(404).json({ message: "Penalty not found" });
     }
 
+    // Update the salary table based on placement with the negative penalty amount
+    if (deletedPenalty.placement === "DeliveryGuy") {
+      await updateDeliveryGuySalaryTable(
+        branch.activeDeliverySalaryTable,
+        deletedPenalty.employeeId,
+        "penalty",
+        -deletedPenalty.amount,
+        -deletedPenalty.amount,
+        session
+      );
+    } else {
+      await updateStaffSalaryTableEntry(
+        branch.activeStaffSalarySheet,
+        deletedPenalty.employeeId,
+        "penalty",
+        -deletedPenalty.amount,
+        -deletedPenalty.amount,
+        session
+      );
+    }
+
+    // Commit the transaction if successful
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(200).json({ message: "Penalty deleted successfully" });
   } catch (error) {
+    // Abort the transaction and end the session in case of an error
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
