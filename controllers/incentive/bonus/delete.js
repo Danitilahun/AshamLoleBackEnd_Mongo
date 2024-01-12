b07b9e07149852f545f794d511d445f05e66bd43
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-12 18:03:30 +0300

    feat: add session and transaction support to createBonus and editBonus
    This commit introduces session and transaction handling for MongoDB operations in the createBonus and editBonus functions. Sessions are now used to ensure atomicity and consistency during database operations. The code has been updated to start, commit, and end the session appropriately, providing better error handling and data integrity.

--- controllers/incentive/bonus/delete.js
+++ controllers/incentive/bonus/delete.js
@@ -1,17 +1,32 @@
+const { startSession } = require("mongoose");
 const Bonus = require("../../../models/incentive/bonusSchema");
 
 // Delete a bonus
 const deleteBonus = async (req, res) => {
+  const session = await startSession();
+  session.startTransaction();
+
   try {
     const { id } = req.params;
-    const deletedBonus = await Bonus.findByIdAndDelete(id);
+    const deletedBonus = await Bonus.findByIdAndDelete(id).session(session);
 
     if (!deletedBonus) {
+      // Abort the transaction and end the session if bonus not found
+      await session.abortTransaction();
+      session.endSession();
       return res.status(404).json({ message: "Bonus not found" });
     }
 
+    // Commit the transaction if successful
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(200).json({ message: "Bonus deleted successfully" });
   } catch (error) {
+    // Abort the transaction and end the session in case of an error
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
