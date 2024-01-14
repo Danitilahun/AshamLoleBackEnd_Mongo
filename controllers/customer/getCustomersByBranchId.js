68b845a7b74da7b442d7267d2741cd9b1a2e93e0
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-15 01:01:08 +0300

    feat: implement paginated customer retrieval by branchId

--- controllers/customer/getCustomersByBranchId.js
+++ controllers/customer/getCustomersByBranchId.js
@@ -2,8 +2,14 @@ const Customer = require("../../models/customerSchema");
 
 const getCustomersByBranchId = async (req, res) => {
   const { branchId } = req.params;
+  const { page = 1, pageSize = 10 } = req.query;
+
   try {
-    const customers = await Customer.find({ branchId });
+    const skip = (page - 1) * pageSize;
+    const customers = await Customer.find({ branchId })
+      .skip(skip)
+      .limit(Number(pageSize));
+
     res.status(200).json(customers);
   } catch (error) {
     console.error("Error fetching customers by branchId:", error);
