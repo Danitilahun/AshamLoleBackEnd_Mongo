20736e40ad31fc39031856fa4d5443e568782e49
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-14 18:48:12 +0300

    feat: add controller function to retrieve sheets by branchId
    Created a new Express.js controller function to fetch all sheets associated with a specific branchId. This function queries the database and returns the results in the response. This enhances the API by providing a way to retrieve sheets based on a specific branch.

---
+++ controllers/sheet/getAllSheetsByBranchId.js
@@ -0,0 +1,17 @@
+const Sheet = require("../../models/sheetsSchema");
+
+// Controller function to get all sheets by branchId
+const getAllSheetsByBranchId = async (req, res) => {
+  const { branchId } = req.params;
+
+  try {
+    const sheets = await Sheet.find({ branchId });
+
+    res.status(200).json({ success: true, sheets });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+};
+
+module.exports = getAllSheetsByBranchId;
--- models/sheetsSchema.js
+++ models/sheetsSchema.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const SheetsSchema = new Schema({
-  Tables: [String],
-  active: String,
-  activeDailySummary: String,
-  branchId: String,
-  createdAt: { type: Date, default: Date.now },
-  date: String,
-  name: String,
-  prevActive: String,
-  previousActive: String,
-  realDate: String,
-  sheetNumber: Number,
-  sheetStatus: String,
-  tableDate: [String],
-  tableCount: Number,
-});
+const SheetsSchema = new Schema(
+  {
+    Tables: [String],
+    activeDailySummery: String,
+    activeDGSummery: String,
+    branchId: String,
+    date: String,
+    name: String,
+    previousActive: String,
+    realDate: String,
+    sheetNumber: Number,
+    sheetStatus: String,
+    tableCount: Number,
+  },
+  { timestamps: true }
+);
 
 const Sheet = mongoose.model("Sheets", SheetsSchema);
 
