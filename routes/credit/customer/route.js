6c56b82d0dc768724920b8435993df6bcee9aec4
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-17 20:45:12 +0300

    feat: customer credit crud finished

--- routes/credit/customer/route.js
+++ routes/credit/customer/route.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const createCredit = require("../../../controllers/credit/customerCredit/create");
+const editCredit = require("../../../controllers/credit/customerCredit/edit");
+const deleteCredit = require("../../../controllers/credit/customerCredit/delete");
+const getCustomerCreditsByBranchId = require("../../../controllers/credit/customerCredit/getByBranchId");
 const router = express.Router();
 
 // Define the route for creating data for an admin
-router.post("/");
+// Get all expenses
+router.get("/", getCustomerCreditsByBranchId);
+
+// Create a new expense
+router.post("/", createCredit);
+
+// Update an existing expense
+router.put("/:id", editCredit);
+
+// Delete an expense
+router.delete("/:id", deleteCredit);
 
 module.exports = router;
