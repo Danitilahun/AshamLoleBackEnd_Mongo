eb3d86a21055de7f3449ed3b2fcc27e2300266f8
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-17 21:33:40 +0300

    feat: finance credit crud finished

--- routes/credit/staff/route.js
+++ routes/credit/staff/route.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const createStaffCredit = require("../../../controllers/credit/staffCredit/create");
+const editStaffCredit = require("../../../controllers/credit/staffCredit/edit");
+const deleteCredit = require("../../../controllers/credit/staffCredit/delete");
+const getStaffCreditsByBranchId = require("../../../controllers/credit/staffCredit/getByBranchId");
 const router = express.Router();
 
 // Define the route for creating data for an admin
-router.post("/");
+router.get("/", getStaffCreditsByBranchId);
+
+// Create a new expense
+router.post("/", createStaffCredit);
+
+// Update an existing expense
+router.put("/:id", editStaffCredit);
+
+// Delete an expense
+router.delete("/:id", deleteCredit);
 
 module.exports = router;
