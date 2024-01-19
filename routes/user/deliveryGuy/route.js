b59b97c6b6bc84830dd89921659da81bf52b9e1c
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 21:56:58 +0300

    feat: Add route for completing a delivery task
    - Integrate endpoint for completing a delivery task within the delivery guy routes
    - Implement the  controller to handle the completion of a task
    - Ensure proper handling of the task completion process in the controller

--- routes/user/deliveryGuy/route.js
+++ routes/user/deliveryGuy/route.js
@@ -5,18 +5,17 @@ const deleteDeliveryGuy = require("../../../controllers/users/deliveryGuy/delete
 const getDeliveryguyById = require("../../../controllers/users/deliveryGuy/get");
 const handlePayController = require("../../../controllers/users/deliveryGuy/handlePayController");
 const updateDeliveryGuyActiveness = require("../../../controllers/users/deliveryGuy/updateDeliveryGuyActiveness");
+const completeTask = require("../../../controllers/users/deliveryGuy/CompleteTask");
 
 const router = express.Router();
 
 // Define the route for creating data for an admin
 // Get all expenses
 router.get("/", getDeliveryguyById);
-
 // Create a new expense
 router.post("/", createDeliveryGuy);
-
+router.post("/complete", completeTask);
 // Update an existing expense
-
 router.put("/:id", editDeliveryGuy);
 router.put("/pay/:id", handlePayController);
 router.put("/activeness/:id", updateDeliveryGuyActiveness);
