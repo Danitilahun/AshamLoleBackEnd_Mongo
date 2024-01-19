0cdf3cb9841b5f57e5af18839d734ee18df6183e
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 21:18:24 +0300

    feat: Implement pagination in getCardDistributeByBranchAndSheet route
    - Introduce pagination using page and limit parameters
    - Retrieve CardDistribute records based on branchId and sheetId with pagination
    - Update response to include paginated CardDistribute records

--- controllers/report/get/getCardDistributeByBranchAndSheet.js
+++ controllers/report/get/getCardDistributeByBranchAndSheet.js
@@ -1,10 +1,21 @@
 const CardDistribute = require("../../../models/report/cardDistributeSchema");
 
-// Controller function to fetch CardDistribute by branchId and sheetId
+// Controller function to fetch CardDistribute by branchId and sheetId with pagination
 const getCardDistributeByBranchAndSheet = async (req, res) => {
   try {
-    const { branchId, sheetId } = req.body;
-    const cardDistributeData = await CardDistribute.find({ branchId, sheetId });
+    const { branchId, sheetId, page = 1, limit = 10 } = req.query;
+
+    // Convert page and limit to numbers
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+
+    // Calculate skip value for pagination
+    const skip = (pageNumber - 1) * limitNumber;
+
+    const cardDistributeData = await CardDistribute.find({ branchId, sheetId })
+      .skip(skip)
+      .limit(limitNumber);
+
     res.json(cardDistributeData);
   } catch (error) {
     console.error("Error in fetching CardDistribute:", error);
