2b598bb34fd4875caa62650f41a951e38a0f085c
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 21:21:17 +0300

    feat: Implement pagination in getWifiDistributeByBranchAndSheet route
    - Introduce pagination using page and limit parameters
    - Retrieve paginated WifiDistribute records based on branchId and sheetId
    - Update response to include paginated WifiDistribute records

--- controllers/report/get/getWifiDistributeByBranchAndSheet.js
+++ controllers/report/get/getWifiDistributeByBranchAndSheet.js
@@ -1,14 +1,32 @@
 const WifiDistribute = require("../../../models/report/wifiDistributeSchema");
 
-// Controller function to fetch WifiDistribute by branchId and sheetId
+// Controller function to fetch paginated WifiDistribute by branchId and sheetId
 const getWifiDistributeByBranchAndSheet = async (req, res) => {
   try {
-    const { branchId, sheetId } = req.body;
-    const wifiDistributeData = await WifiDistribute.find({ branchId, sheetId });
-    res.json(wifiDistributeData);
+    const { branchId, sheetId, page = 1, limit = 10 } = req.query;
+
+    // Convert page and limit to numbers
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+
+    // Calculate skip value for pagination
+    const skip = (pageNumber - 1) * limitNumber;
+
+    if (!branchId || !sheetId) {
+      return res
+        .status(400)
+        .json({ error: "Both branchId and sheetId are required." });
+    }
+
+    // Fetch paginated WifiDistribute records matching the provided branchId and sheetId
+    const wifiDistributeData = await WifiDistribute.find({ branchId, sheetId })
+      .skip(skip)
+      .limit(limitNumber);
+
+    return res.json(wifiDistributeData);
   } catch (error) {
     console.error("Error in fetching WifiDistribute:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
