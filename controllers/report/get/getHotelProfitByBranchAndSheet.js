31fd1fd04d21496ad536394fd80023cfdbeee1b2
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 21:19:53 +0300

    feat: Implement pagination in getHotelProfitByBranchAndSheet route
    - Introduce pagination using page and limit parameters
    - Retrieve paginated HotelProfit records based on branchId and sheetId
    - Update response to include paginated HotelProfit records

--- controllers/report/get/getHotelProfitByBranchAndSheet.js
+++ controllers/report/get/getHotelProfitByBranchAndSheet.js
@@ -1,14 +1,32 @@
 const HotelProfit = require("../../../models/report/hotelProfitSchema");
 
-// Controller function to fetch HotelProfit by branchId and sheetId
+// Controller function to fetch paginated HotelProfit by branchId and sheetId
 const getHotelProfitByBranchAndSheet = async (req, res) => {
   try {
-    const { branchId, sheetId } = req.body;
-    const hotelProfitData = await HotelProfit.find({ branchId, sheetId });
-    res.json(hotelProfitData);
+    const { branchId, sheetId, page = 1, limit = 10 } = req.query;
+
+    // Convert page and limit to numbers
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+
+    // Calculate skip value for pagination
+    const skip = (pageNumber - 1) * limitNumber;
+
+    if (!branchId || !sheetId) {
+      return res
+        .status(400)
+        .json({ error: "Both branchId and sheetId are required." });
+    }
+
+    // Fetch paginated HotelProfit records matching the provided branchId and sheetId
+    const hotelProfitData = await HotelProfit.find({ branchId, sheetId })
+      .skip(skip)
+      .limit(limitNumber);
+
+    return res.json(hotelProfitData);
   } catch (error) {
     console.error("Error in fetching HotelProfit:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
