f6e2254eba93915fc5da81e9023448d7ae92d8ae
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 22:10:33 +0300

    feat: Implement routes for creating, editing, and getting Superadmin

---
+++ routes/user/superAdmin/route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const createSuperadmin = require("../../../controllers/users/superAdmin/create");
+const editSuperadmin = require("../../../controllers/users/superAdmin/edit");
+const getSuperAdmin = require("../../../controllers/users/superAdmin/get");
+
+const router = express.Router();
+
+// Define the route for creating data for an admin
+
+router.get("/", getSuperAdmin);
+router.post("/", createSuperadmin);
+router.put("/:id", editSuperadmin);
+
+module.exports = router;
