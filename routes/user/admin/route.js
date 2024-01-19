9afb3e682bf8505f8ae6ece6f9bfd2e4c31f7530
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 21:42:24 +0300

    feat: adding getAllAdmin in the admin route

--- routes/user/admin/route.js
+++ routes/user/admin/route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const createAdmin = require("../../../controllers/users/admin/create");
 const editAdmin = require("../../../controllers/users/admin/edit");
 const deleteAdmin = require("../../../controllers/users/admin/delete");
+const getAllAdmin = require("../../../controllers/users/admin/get");
 
 const router = express.Router();
 
