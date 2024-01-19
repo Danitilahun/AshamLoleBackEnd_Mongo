9692be24ca6ccfd51fa1361798bc1e2ceafb684f
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 20:36:13 +0300

    feat: Implement Asbeza Order routes
    - Create route to get all Asbeza Orders by date
    - Implement route for creating new Customer and Asbeza Orders
    - Add route for updating existing Customer and Asbeza Orders
    - Introduce route for deleting Asbeza Orders and updating associated Customer information

---
+++ routes/order/asbeza/route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const createHolidayBonus = require("./holiday/route");
+const createCustomerAndAsbeza = require("../../../controllers/order/Asbeza/create");
+const editCustomerAndAsbeza = require("../../../controllers/order/Asbeza/update");
+const deleteAsbezaAndUpdateCustomer = require("../../../controllers/order/Asbeza/delete");
+const getAllAsbezaByDate = require("../../../controllers/order/Asbeza/get");
+
+router.get("/", getAllAsbezaByDate);
+
+router.post("/", createCustomerAndAsbeza);
+
+router.put("/:id", editCustomerAndAsbeza);
+
+router.delete("/:id", deleteAsbezaAndUpdateCustomer);
+
+module.exports = router;
