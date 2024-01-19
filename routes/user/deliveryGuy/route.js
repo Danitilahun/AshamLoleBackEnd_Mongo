d87fa0e8dd9450d623d2745829eb685e7851614e
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 21:55:33 +0300

    feat: Implement CRUD operations for delivery guys
    - Introduce endpoint for fetching a delivery guy by ID
    - Create route for adding a new delivery guy
    - Implement route for updating an existing delivery guy
    - Add route for handling payment for a delivery guy
    - Introduce route for updating the activeness status of a delivery guy
    - Add route for deleting a delivery guy by ID

---
+++ routes/user/deliveryGuy/route.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const createDeliveryGuy = require("../../../controllers/users/deliveryGuy/create");
+const editDeliveryGuy = require("../../../controllers/users/deliveryGuy/edit");
+const deleteDeliveryGuy = require("../../../controllers/users/deliveryGuy/delete");
+const getDeliveryguyById = require("../../../controllers/users/deliveryGuy/get");
+const handlePayController = require("../../../controllers/users/deliveryGuy/handlePayController");
+const updateDeliveryGuyActiveness = require("../../../controllers/users/deliveryGuy/updateDeliveryGuyActiveness");
+
+const router = express.Router();
+
+// Define the route for creating data for an admin
+// Get all expenses
+router.get("/", getDeliveryguyById);
+
+// Create a new expense
+router.post("/", createDeliveryGuy);
+
+// Update an existing expense
+
+router.put("/:id", editDeliveryGuy);
+router.put("/pay/:id", handlePayController);
+router.put("/activeness/:id", updateDeliveryGuyActiveness);
+// Delete an expense
+router.delete("/:id", deleteDeliveryGuy);
+
+module.exports = router;
