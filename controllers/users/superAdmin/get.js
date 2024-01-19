4697c86358465bccfd7e2211fd2df1697a0a2c62
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 22:07:56 +0300

    feat: Implement controller to get the single superadmin

---
+++ controllers/users/superAdmin/get.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Superadmin = require("../../../models/users/superadmin");
+
+// Controller function to get the single superadmin
+const getSuperAdmin = async (req, res) => {
+  try {
+    // Fetch the single superadmin document
+    const superadmin = await Superadmin.findOne();
+
+    // Check if superadmin document exists
+    if (!superadmin) {
+      return res.status(404).json({ message: "Superadmin not found" });
+    }
+
+    res.status(200).json({ superadmin });
+  } catch (error) {
+    console.error("Error in getSuperAdmin:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = getSuperAdmin;
