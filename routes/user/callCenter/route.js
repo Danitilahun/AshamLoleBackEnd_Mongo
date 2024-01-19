653baee4e740f97d0e3f358d16c7c1bedc02e298
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-19 21:47:33 +0300

    feat: Implement CRUD operations for call center employees
    - Introduce endpoint for fetching all call center employees
    - Create route for adding a new call center employee
    - Implement route for updating an existing call center employee
    - Add route for deleting a call center employee by ID

--- routes/user/callCenter/route.js
+++ routes/user/callCenter/route.js
@@ -7,5 +7,16 @@ const getAllCallCenter = require("../../../controllers/users/callcenter/get");
 const router = express.Router();
 
 // Define the route for creating data for an admin
+// Get all expenses
+router.get("/", getAllCallCenter);
+
+// Create a new expense
+router.post("/", createCallCenterEmployee);
+
+// Update an existing expense
+router.put("/:id", editCallCenterEmployee);
+
+// Delete an expense
+router.delete("/:id", deleteCallCenterEmployee);
 
 module.exports = router;
