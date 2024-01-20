945c2f0e6f9c341aaba835c9eba34c2cd298d1c4
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 22:04:43 +0300

    feat: Implement deleteAdmin function with session and transaction support
    - Use Mongoose session for the delete operation to ensure data integrity
    - Provide transactional support for the deleteAdmin operation

--- controllers/users/admin/delete.js
+++ controllers/users/admin/delete.js
@@ -1,17 +1,29 @@
+const mongoose = require("mongoose");
 const Admin = require("../../../models/user/adminSchema");
 
 // Delete an admin
 const deleteAdmin = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const { id } = req.params;
-    const deletedAdmin = await Admin.findByIdAndDelete(id);
+    const deletedAdmin = await Admin.findByIdAndDelete(id).session(session);
 
     if (!deletedAdmin) {
+      await session.abortTransaction();
+      session.endSession();
       return res.status(404).json({ message: "Admin not found" });
     }
 
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(200).json({ message: "Admin deleted successfully" });
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
