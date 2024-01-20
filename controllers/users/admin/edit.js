8d0ff6400fa7aa4b8388b59afc04fbd5f3106a1a
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 22:08:59 +0300

    feat: Implement editAdmin function with session and transaction support
    - Utilize Mongoose session for the update operation to ensure atomicity
    - Provide transactional support for the editAdmin operation

--- controllers/users/admin/edit.js
+++ controllers/users/admin/edit.js
@@ -1,19 +1,34 @@
+const mongoose = require("mongoose");
 const Admin = require("../../../models/user/adminSchema");
 
 // Edit an existing admin
 const editAdmin = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const { id } = req.params;
     const data = req.body;
 
-    const updatedAdmin = await Admin.findByIdAndUpdate(id, data, { new: true });
+    const updatedAdmin = await Admin.findByIdAndUpdate(id, data, {
+      new: true,
+      session,
+    });
 
     if (!updatedAdmin) {
+      await session.abortTransaction();
+      session.endSession();
       return res.status(404).json({ message: "Admin not found" });
     }
 
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(200).json(updatedAdmin);
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
