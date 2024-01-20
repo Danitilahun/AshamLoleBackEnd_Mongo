baf74c8bcdee23a39b11ddb1c2a724941d757f66
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 20:43:57 +0300

    feat: Implement createCallCenterEmployee function with transaction
    This commit introduces the createCallCenterEmployee function, which creates a new call center employee with the provided data. The function utilizes Mongoose transactions to ensure data integrity during the save operation.

--- controllers/users/callcenter/create.js
+++ controllers/users/callcenter/create.js
@@ -1,15 +1,28 @@
+const mongoose = require("mongoose");
 const CallCenter = require("../../../models/user/callCenterSchema");
 
 // Create a new call center employee
 const createCallCenterEmployee = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const data = req.body;
 
     const newCallCenterEmployee = new CallCenter(data);
 
-    const savedCallCenterEmployee = await newCallCenterEmployee.save();
+    const savedCallCenterEmployee = await newCallCenterEmployee.save({
+      session,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(201).json(savedCallCenterEmployee);
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
