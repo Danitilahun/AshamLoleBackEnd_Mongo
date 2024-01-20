51f1acaff2490d849f2921683f385ae32550aec5
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 21:29:31 +0300

    feat: Implement createDeliveryGuy function with transaction
    - Create a new delivery guy using data from the request body
    - Utilize Mongoose transactions to ensure data integrity during the save operation

--- controllers/users/deliveryGuy/create.js
+++ controllers/users/deliveryGuy/create.js
@@ -1,15 +1,26 @@
+const mongoose = require("mongoose");
 const Deliveryguy = require("../../../models/deliveryguySchema");
 
 // Create a new delivery guy
 const createDeliveryGuy = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const data = req.body;
 
     const newDeliveryGuy = new Deliveryguy(data);
 
-    const savedDeliveryGuy = await newDeliveryGuy.save();
+    const savedDeliveryGuy = await newDeliveryGuy.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(201).json(savedDeliveryGuy);
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
