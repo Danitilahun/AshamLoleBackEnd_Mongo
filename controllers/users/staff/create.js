8f50c41bb9f2de2c6929263cf20b8d907bec8bf0
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 19:39:09 +0300

    feat: update create staff structure

--- controllers/users/staff/create.js
+++ controllers/users/staff/create.js
@@ -3,35 +3,9 @@ const Staff = require("../../../models/staffSchema");
 // Create a new staff member
 const createStaffMember = async (req, res) => {
   try {
-    const {
-      bankAccount,
-      branchId,
-      fullAddress,
-      fullName,
-      paid,
-      phone,
-      profileImage,
-      salary,
-      securityAddress,
-      securityName,
-      securityPhone,
-      uniqueName,
-    } = req.body;
+    const data = req.body;
 
-    const newStaffMember = new Staff({
-      bankAccount,
-      branchId,
-      fullAddress,
-      fullName,
-      paid,
-      phone,
-      profileImage,
-      salary,
-      securityAddress,
-      securityName,
-      securityPhone,
-      uniqueName,
-    });
+    const newStaffMember = new Staff(data);
 
     const savedStaffMember = await newStaffMember.save();
     res.status(201).json(savedStaffMember);
