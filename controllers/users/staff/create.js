1c90d40c7deb58b7917250cb96702308b3d44641
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 21:28:14 +0300

    feat: Implement createStaffMember function with transaction
    - Create a new staff member using data from the request body
    - Utilize Mongoose transactions to ensure data integrity during the save operation

--- controllers/users/staff/create.js
+++ controllers/users/staff/create.js
@@ -1,15 +1,26 @@
+const mongoose = require("mongoose");
 const Staff = require("../../../models/staffSchema");
 
 // Create a new staff member
 const createStaffMember = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const data = req.body;
 
     const newStaffMember = new Staff(data);
 
-    const savedStaffMember = await newStaffMember.save();
+    const savedStaffMember = await newStaffMember.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(201).json(savedStaffMember);
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
