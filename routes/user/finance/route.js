75382e670cf00c5cd9f9431e242bec1cd739b399
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-21 01:27:41 +0300

    feat: update finance route after delete finance functionality is removed

--- routes/user/finance/route.js
+++ routes/user/finance/route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const createFinanceEntry = require("../../../controllers/users/finance/create");
 const editFinanceEntry = require("../../../controllers/users/finance/edit");
 const getAllFinance = require("../../../controllers/users/finance/get");
-const deleteFinanceEntry = require("../../../controllers/users/finance/delete");
 
 const router = express.Router();
 
@@ -12,7 +11,5 @@ router.get("/", getAllFinance);
 // Create a new expense
 router.post("/", createFinanceEntry);
 router.put("/:id", editFinanceEntry);
-
-router.delete("/:id", deleteFinanceEntry);
 
 module.exports = router;
