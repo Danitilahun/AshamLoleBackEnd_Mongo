6d188bfbc5c042176c89aab0f01316d17ea1aa94
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 19:38:37 +0300

    feat: update create finance structure

--- controllers/users/finance/create.js
+++ controllers/users/finance/create.js
@@ -3,46 +3,9 @@ const Finance = require("../../../models/user/financeschema");
 // Create a new finance entry
 const createFinanceEntry = async (req, res) => {
   try {
-    const {
-      BudgetSummery,
-      balance,
-      bank,
-      bankAccount,
-      budget,
-      credit,
-      disable,
-      email,
-      fullAddress,
-      fullName,
-      phone,
-      profileImage,
-      salary,
-      securityAddress,
-      securityName,
-      securityPhone,
-      totalExpense,
-    } = req.body;
+    const data = req.body;
 
-    const newFinanceEntry = new Finance({
-      BudgetSummery,
-      balance,
-      bank,
-      bankAccount,
-      budget,
-      credit,
-      disable,
-      email,
-      fullAddress,
-      fullName,
-      phone,
-      profileImage,
-      salary,
-      securityAddress,
-      securityName,
-      securityPhone,
-      totalExpense,
-      updatedAt: new Date(),
-    });
+    const newFinanceEntry = new Finance(data);
 
     const savedFinanceEntry = await newFinanceEntry.save();
     res.status(201).json(savedFinanceEntry);
