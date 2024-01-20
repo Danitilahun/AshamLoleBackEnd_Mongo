093456a660d790aa51ed6400be3a447e96b4f7e5
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 20:51:59 +0300

    feat: Implement createFinanceEntry function with transaction
    This commit introduces the createFinanceEntry function, which creates a new finance entry using data from the request body. The function utilizes Mongoose transactions to ensure data integrity during the save operation.

--- controllers/users/finance/create.js
+++ controllers/users/finance/create.js
@@ -1,15 +1,26 @@
+const mongoose = require("mongoose");
 const Finance = require("../../../models/user/financeschema");
 
 // Create a new finance entry
 const createFinanceEntry = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const data = req.body;
 
     const newFinanceEntry = new Finance(data);
 
-    const savedFinanceEntry = await newFinanceEntry.save();
+    const savedFinanceEntry = await newFinanceEntry.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(201).json(savedFinanceEntry);
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
