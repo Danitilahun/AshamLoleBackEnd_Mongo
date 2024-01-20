a8fc5445d5f2aaa18fc3f4f738ff4ac2bb30a724
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-21 01:46:42 +0300

    feat: Implement function to create activation token using JWT
    - Created 'createActivationToken' function to generate JWT activation tokens.
    - Utilized the 'jsonwebtoken' library to sign user data with a secret and set expiration.
    - Configured token expiration to 5 minutes.
    - Used environment variables for sensitive information.

---
+++ util/createActivationToken.js
@@ -0,0 +1,8 @@
+const jwt = require("jsonwebtoken");
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
+
+module.exports = createActivationToken;
