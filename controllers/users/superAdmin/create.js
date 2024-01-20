f19dc1073dc6c8ca873bc9df8e29e9d54f79f62f
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 21:22:41 +0300

    feat: Implement createSuperadmin function with transaction
    This commit introduces the createSuperadmin function, which creates a new Superadmin using data from the request body. The function utilizes Mongoose transactions to ensure data integrity during the save operation.

--- controllers/users/superAdmin/create.js
+++ controllers/users/superAdmin/create.js
@@ -1,15 +1,26 @@
+const mongoose = require("mongoose");
 const Superadmin = require("../../../models/user/superadminSchema");
 
 // Create a new superadmin
 const createSuperadmin = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     const data = req.body;
 
     const newSuperadmin = new Superadmin(data);
 
-    const savedSuperadmin = await newSuperadmin.save();
+    const savedSuperadmin = await newSuperadmin.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
     res.status(201).json(savedSuperadmin);
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({ message: error.message });
   }
 };
