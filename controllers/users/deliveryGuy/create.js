0adafeff58e8617627f4d80e7fb19f1c81027549
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-20 19:37:39 +0300

    feat: update create delivery guy structure

--- controllers/users/deliveryGuy/create.js
+++ controllers/users/deliveryGuy/create.js
@@ -3,33 +3,9 @@ const Deliveryguy = require("../../../models/deliveryguySchema");
 // Create a new delivery guy
 const createDeliveryGuy = async (req, res) => {
   try {
-    const {
-      activeness,
-      bankAccount,
-      branchId,
-      fullAddress,
-      fullName,
-      phone,
-      profileImage,
-      securityAddress,
-      securityName,
-      securityPhone,
-      uniqueName,
-    } = req.body;
+    const data = req.body;
 
-    const newDeliveryGuy = new Deliveryguy({
-      activeness,
-      bankAccount,
-      branchId,
-      fullAddress,
-      fullName,
-      phone,
-      profileImage,
-      securityAddress,
-      securityName,
-      securityPhone,
-      uniqueName,
-    });
+    const newDeliveryGuy = new Deliveryguy(data);
 
     const savedDeliveryGuy = await newDeliveryGuy.save();
     res.status(201).json(savedDeliveryGuy);
