381719b314f8804449ba16845c3d5b443fa1fdd0
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-21 15:24:55 +0300

    feat: fix buges in staff route

--- routes/report/cardFee/route.js
+++ routes/report/cardFee/route.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const createCardFeeAndDailyCredit = require("../../../controllers/report/cardFee/cardFee");
 const recordReturnCard = require("../../../controllers/report/cardFee/returnedcard");
-const {
-  getCardFeesByBranchAndSheet,
-} = require("../../../controllers/report/get/getCardFeesByBranchAndSheet");
+const getCardFeesByBranchAndSheet = require("../../../controllers/report/get/getCardFeesByBranchAndSheet");
 const router = express.Router();
 
 router.get("/", getCardFeesByBranchAndSheet);
