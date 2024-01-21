66be77753c7c10d4e4d497b36c0128a307d75318
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-22 00:12:05 +0300

    feat: update route for superAdmin

--- routes/user/superAdmin/route.js
+++ routes/user/superAdmin/route.js
@@ -2,12 +2,11 @@ const express = require("express");
 const createSuperadmin = require("../../../controllers/users/superAdmin/create");
 const editSuperadmin = require("../../../controllers/users/superAdmin/edit");
 const getSuperAdmin = require("../../../controllers/users/superAdmin/get");
-const validateSuperadmin = require("../../../middlewares/validation/validateSuperadmin");
 
 const router = express.Router();
 
 router.get("/", getSuperAdmin);
-router.post("/", validateSuperadmin, createSuperadmin);
+router.post("/", createSuperadmin);
 router.put("/:id", editSuperadmin);
 
 module.exports = router;
