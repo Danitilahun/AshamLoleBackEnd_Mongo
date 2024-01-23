4e2bcc6e9cbccc2995e47123cff7e3e6d2b24025
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-23 21:33:26 +0300

    refactor: add role for sheet crud

--- routes/api.js
+++ routes/api.js
@@ -76,7 +76,11 @@ router.use(
   verifyRoles(process.env.SUPERADMIN, process.env.ADMIN),
   IncentiveRoute
 );
-router.use("/sheet", SheetRoute);
+router.use(
+  "/sheet",
+  verifyRoles(process.env.SUPERADMIN, process.env.ADMIN),
+  SheetRoute
+);
 router.use("/gainprice", GainPriceRoute);
 
 module.exports = router;
