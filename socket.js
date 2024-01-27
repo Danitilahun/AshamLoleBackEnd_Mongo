c8b00f66e3a4a07d1ec4a727d9a95c1ba2487013
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-27 16:22:50 +0300

    chore: Implement Socket.IO setup for real-time communication
    Create a module to set up Socket.IO on the server, enabling real-time communication with connected clients. The setup includes handling events such as 'getAllBranches', 'getAllAdmins', 'getAllCallCenters', 'getAllDeliveryGuys', 'getAllFinances', and 'getAllStaffMembers'. Socket.IO is configured to allow connections from http://localhost:3000.

--- socket.js
+++ socket.js
@@ -1,5 +1,10 @@
 const { Server } = require("socket.io");
 const getAllBranches = require("./events/getAllBranches");
+const getAllAdmins = require("./events/getAllAdmins");
+const getAllCallCenters = require("./events/getAllCallCenters");
+const getAllDeliveryGuys = require("./events/getAllDeliveryGuys");
+const getAllFinances = require("./events/getAllFinances");
+const getAllStaffMembers = require("./events/getAllStaffMembers");
 
 let io;
 
@@ -15,6 +20,11 @@ const setupSocketIO = (server) => {
     console.log("a user connected");
 
     socket.on("getAllBranches", () => getAllBranches(socket));
+    socket.on("getAllAdmins", () => getAllAdmins(socket));
+    socket.on("getAllCallCenters", () => getAllCallCenters(socket));
+    socket.on("getAllDeliveryGuys", () => getAllDeliveryGuys(socket));
+    socket.on("getAllFinances", () => getAllFinances(socket));
+    socket.on("getAllStaffMembers", () => getAllStaffMembers(socket));
 
     socket.on("disconnect", () => {
       console.log("user disconnected");
