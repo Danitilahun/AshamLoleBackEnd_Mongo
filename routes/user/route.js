9feecb2b5329894a3f20a3130a8f58152536fc2f
Daniel Tilahun <tiledan2015@gmail.com>
2024-01-28 22:13:19 +0300

    refactor: If email is null or undefined, move to the next middleware

--- routes/user/route.js
+++ routes/user/route.js
@@ -8,9 +8,20 @@ const deliveryGuyRoute = require("./deliveryGuy/route");
 const superAdminRoute = require("./superAdmin/route");
 const staffRoute = require("./staff/route");
 const verifyRoles = require("../../middlewares/verifyRoles");
+const checkDuplicateEmail = require("../../middlewares/checkDuplicateEmail");
 
-router.use("/admin", verifyRoles(process.env.SUPERADMIN), adminRoute);
-router.use("/callCenter", verifyRoles(process.env.SUPERADMIN), callCenterRoute);
+router.use(
+  "/admin",
+  verifyRoles(process.env.SUPERADMIN),
+  checkDuplicateEmail,
+  adminRoute
+);
+router.use(
+  "/callCenter",
+  verifyRoles(process.env.SUPERADMIN),
+  checkDuplicateEmail,
+  callCenterRoute
+);
 router.use("/finance", financeRoute);
 router.use("/deliveryGuy", deliveryGuyRoute);
 router.use("/superAdmin", superAdminRoute);
