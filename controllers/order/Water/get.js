acdf6090ed462759f31dc3f74eb75c914acc9094
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-12 01:52:54 +0300

    refactor: Modify getAllWaterByDate to match structure of getAllAsbezaByDateAndBranch

--- controllers/order/Water/get.js
+++ controllers/order/Water/get.js
@@ -2,15 +2,31 @@ const Water = require("../../../models/service/waterSchema");
 
 const getAllWaterByDate = async (req, res) => {
   try {
-    const { date } = req.body;
+    const { date, page = 1 } = req.query;
+    const limit = 10;
 
-    // Fetch all Water records matching the provided date
-    const waterRecords = await Water.find({ date: { $eq: date } });
+    // Convert page to a number
+    const pageNumber = parseInt(page);
 
-    res.status(200).json({
-      message: "Water records retrieved successfully",
-      waterRecords,
-    });
+    // Calculate skip value for pagination
+    const skip = (pageNumber - 1) * limit;
+
+    // Construct query object with date
+    const query = {
+      date: { $eq: date },
+    };
+
+    // Fetch Water records matching the provided date with pagination
+    let waterRecords = await Water.find(query).skip(skip).limit(limit);
+
+    // Add rollNumber field in each water record by incrementing
+    waterRecords = waterRecords.map((water, index) => ({
+      ...water.toObject(),
+      rollNumber: skip + index + 1,
+    }));
+
+    console.log("Water Records:", waterRecords);
+    res.status(200).json(waterRecords);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
