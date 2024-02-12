79f7ff809dfcc2f3420e47d6d33c11a057971563
Daniel Tilahun <tiledan2015@gmail.com>
2024-02-13 01:50:16 +0300

    feat: Implement getAllWifiByDateAndCallcenter function, modify variable names and query to use callcenterId

---
+++ controllers/order/Wifi/getAllWifiByDateAndCallcenter.js
@@ -0,0 +1,39 @@
+const Wifi = require("../../../models/service/wifiSchema");
+
+const getAllWifiByDateAndCallcenter = async (req, res) => {
+  try {
+    const { date, callcenterId, page = 1, fromWhere } = req.query;
+    const limit = 10;
+
+    // Convert page to a number
+    const pageNumber = parseInt(page);
+
+    // Calculate skip value for pagination
+    const skip = (pageNumber - 1) * limit;
+
+    // Construct query object with date, callcenterId, and optional fromWhere
+    const query = {
+      date: { $eq: date },
+      callcenterId: { $eq: callcenterId },
+    };
+    if (fromWhere) {
+      query.fromWhere = { $eq: fromWhere };
+    }
+
+    // Fetch WiFi records matching the provided date, callcenter ID, and fromWhere with pagination
+    let wifiRecords = await Wifi.find(query).skip(skip).limit(limit);
+
+    // Add rollNumber field in each WiFi record by incrementing
+    wifiRecords = wifiRecords.map((wifi, index) => ({
+      ...wifi.toObject(),
+      rollNumber: skip + index + 1,
+    }));
+
+    console.log("WiFi Records:", wifiRecords);
+    res.status(200).json(wifiRecords);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = getAllWifiByDateAndCallcenter;
