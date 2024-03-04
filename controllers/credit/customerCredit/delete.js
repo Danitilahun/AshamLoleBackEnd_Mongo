1c2dde7aa3836df583a9c921c9271b50fb4e9d4c
Daniel Tilahun <tiledan2015@gmail.com>
2024-03-04 22:46:45 +0300

    adding customer change

--- controllers/credit/customerCredit/delete.js
+++ controllers/credit/customerCredit/delete.js
@@ -1,47 +1,47 @@
-// const { startSession } = require("mongoose");
-// const CustomerCredit = require("../../../models/credit/customerCreditSchema");
-// const updateCredit = require("../../../services/creditRelated/updateCredit");
+const { startSession } = require("mongoose");
+const CustomerCredit = require("../../../models/credit/customerCreditSchema");
+const updateCredit = require("../../../services/creditRelated/updateCredit");
 
-// const deleteCredit = async (req, res) => {
-//   const session = await startSession();
-//   session.startTransaction();
+const deleteCredit = async (req, res) => {
+  const session = await startSession();
+  session.startTransaction();
 
-//   try {
-//     const { creditId } = req.params;
+  try {
+    const { creditId } = req.params;
 
-//     const creditToDelete = await CustomerCredit.findById(creditId).session(
-//       session
-//     );
-//     if (!creditToDelete) {
-//       throw new Error("Credit document not found for the given ID.");
-//     }
+    const creditToDelete = await CustomerCredit.findById(creditId).session(
+      session
+    );
+    if (!creditToDelete) {
+      throw new Error("Credit document not found for the given ID.");
+    }
 
-//     const negativeAmount = -parseFloat(creditToDelete.amount || 0);
+    const negativeAmount = -parseFloat(creditToDelete.amount || 0);
 
-//     await updateCredit(
-//       creditToDelete.branchId,
-//       "customerCredit",
-//       negativeAmount,
-//       session
-//     );
+    await updateCredit(
+      creditToDelete.branchId,
+      "customerCredit",
+      negativeAmount,
+      session
+    );
 
-//     // Delete the credit document from MongoDB
-//     await CustomerCredit.findByIdAndDelete(creditId).session(session);
+    // Delete the credit document from MongoDB
+    await CustomerCredit.findByIdAndDelete(creditId).session(session);
 
-//     await session.commitTransaction();
-//     session.endSession();
+    await session.commitTransaction();
+    session.endSession();
 
-//     // Respond with a success message
-//     res.status(200).json({ message: `CustomerCredit Deleted successfully.` });
-//   } catch (error) {
-//     console.error(error);
+    // Respond with a success message
+    res.status(200).json({ message: `CustomerCredit Deleted successfully.` });
+  } catch (error) {
+    console.error(error);
 
-//     await session.abortTransaction();
-//     session.endSession();
+    await session.abortTransaction();
+    session.endSession();
 
-//     // Respond with an error message
-//     res.status(500).json({ message: error.message });
-//   }
-// };
+    // Respond with an error message
+    res.status(500).json({ message: error.message });
+  }
+};
 
-// module.exports = deleteCredit;
+module.exports = deleteCredit;
