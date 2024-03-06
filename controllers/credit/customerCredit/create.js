784a39e7009ee81af288d1eb72f11bd407078943
Daniel Tilahun <tiledan2015@gmail.com>
2024-03-06 21:15:29 +0300

    add change

--- controllers/credit/customerCredit/create.js
+++ controllers/credit/customerCredit/create.js
@@ -1,38 +1,38 @@
-const { startSession } = require("mongoose");
-const CustomerCredit = require("../../../models/credit/customerCreditSchema");
-const updateCredit = require("../../../services/creditRelated/updateCredit");
+// const { startSession } = require("mongoose");
+// const CustomerCredit = require("../../../models/credit/customerCreditSchema");
+// const updateCredit = require("../../../services/creditRelated/updateCredit");
 
-const createCredit = async (req, res) => {
-  const session = await startSession();
-  session.startTransaction();
+// const createCredit = async (req, res) => {
+//   const session = await startSession();
+//   session.startTransaction();
 
-  try {
-    const data = req.body;
+//   try {
+//     const data = req.body;
 
-    const newCredit = await CustomerCredit.create([data], { session });
+//     const newCredit = await CustomerCredit.create([data], { session });
 
-    await updateCredit(
-      data.branchId,
-      "customerCredit",
-      parseFloat(data.amount ? data.amount : 0),
-      session
-    );
+//     await updateCredit(
+//       data.branchId,
+//       "customerCredit",
+//       parseFloat(data.amount ? data.amount : 0),
+//       session
+//     );
 
-    await session.commitTransaction();
-    session.endSession();
+//     await session.commitTransaction();
+//     session.endSession();
 
-    res.status(200).json({
-      message: `CustomerCredit Created successfully.`,
-      data: newCredit,
-    });
-  } catch (error) {
-    console.error(error);
+//     res.status(200).json({
+//       message: `CustomerCredit Created successfully.`,
+//       data: newCredit,
+//     });
+//   } catch (error) {
+//     console.error(error);
 
-    await session.abortTransaction();
-    session.endSession();
+//     await session.abortTransaction();
+//     session.endSession();
 
-    res.status(500).json({ message: error.message });
-  }
-};
+//     res.status(500).json({ message: error.message });
+//   }
+// };
 
-module.exports = createCredit;
+// module.exports = createCredit;
